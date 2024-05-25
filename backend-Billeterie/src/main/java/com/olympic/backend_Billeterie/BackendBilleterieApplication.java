f53d97e4e71110033afcc897166a77035f7e8bb9
Getm26 <metiennedev@gmail.com>
2024-05-25 14:26:04 +0200

    Add struct backend

---
+++ backend-Billeterie/src/main/java/com/olympic/backend_Billeterie/BackendBilleterieApplication.java
@@ -0,0 +1,13 @@
+package com.olympic.backend_Billeterie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBilleterieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBilleterieApplication.class, args);
+	}
+
+}
---
+++ backend-Billeterie/src/test/java/com/olympic/backend_Billeterie/BackendBilleterieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olympic.backend_Billeterie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBilleterieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
