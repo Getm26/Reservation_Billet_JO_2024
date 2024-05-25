e194bd426fafb84871e8dc7cf1d8e29b1a82ef99
Getm26 <metiennedev@gmail.com>
2024-05-25 16:23:40 +0200

    add models

---
+++ backend-Billeterie/src/main/java/com/olympic/backend_Billeterie/model/User.java
@@ -0,0 +1,25 @@
+package com.olympic.backend_Billeterie.model;
+
+import java.util.Set;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles;
+
+    // getters and setters
+}
